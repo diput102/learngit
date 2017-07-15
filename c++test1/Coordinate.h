e20b3b4a1fba271e42356219284926be2e205e1a
diput102 <2382634989@qq.com>
2017-07-15 14:44:55 +0800

    Line and Coordinate

---
+++ c++test1/Coordinate.h
@@ -0,0 +1,14 @@
+class Coordinate
+{
+public:
+	Coordinate(int x,int y);
+	~Coordinate();
+	void setX(int x);
+	int getX();
+	void setY(int y);
+	int getY();  
+public:
+	int m_iX;
+	int m_iY;
+
+};
