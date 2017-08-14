17c706fe2d8ecf1aec0158809b8405cdf5cca8c5
diput102 <2382634989@qq.com>
2017-08-14 22:41:02 +0800

    sizeof VTBL

---
+++ VTBL/Shape.cpp
@@ -0,0 +1,14 @@
+#include"Shape.h"
+Shape::Shape()
+{
+	//cout << "Shape()" << endl;
+}
+Shape::~Shape()
+{
+	//cout << "~Shape()" << endl;
+}
+double Shape::calcArea()
+{
+	cout << "calcArea()" << endl;
+	return 0;
+}
---
+++ VTBL/Shape.h
@@ -0,0 +1,13 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include<iostream>
+using namespace std;
+class Shape
+{
+public:
+	Shape();
+	virtual ~Shape();
+	  double calcArea();
+
+};
+#endif
