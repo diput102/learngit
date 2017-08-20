743b0dffa3b9b141e1af0d2ce1443a02feafbd8a
diput102 <2382634989@qq.com>
2017-08-20 15:55:10 +0800

    8_20

---
+++ exception_handler/demo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdlib.h>
+#include"IndexException.h"
+using namespace std;
+/*异常处理
+1、定义一个Exception类 成员函数：printException，析构函数
+2.定义一个IndexException类 成员函数：printException（）
+
+*/
+void test()
+{
+	throw IndexException();
+}
+int main(void) 
+{
+	try
+	{
+		test();
+	}
+	catch (IndexException &e)
+	{
+		e.printException();
+	}
+	system("pause");
+	return 0;
+}
